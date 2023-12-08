04bd41cc466a3a3805394709c7615033f6f24718
Alexander Kostrovsky <alkstr@mail.ru>
2023-12-08 14:40:19 +0500

    refactor: cracker class

--- Coder/Core/CaesarCipher/Cracker.cs
+++ Coder/Core/CaesarCipher/Cracker.cs
@@ -7,44 +7,47 @@ namespace Coder.Core.CaesarCipher;
 public static class Cracker
 {
     // finds most possible shift
-    public static string Crack(string text)
+    public static (int, string) Crack(string text)
     {
-        var (shift, prob) = (0, double.MaxValue);
-        for (var s = 0; s < Utilities.LowerRussianAlphabet.Length; s++)
-        {
-            var p = CalculateShiftValueProbability(text, s);
-            if (p < prob)
-            {
-                (shift, prob) = (s, p);
-            }
-        }
+        // range from 0 to alphabet length
+        var shiftWithMinSum = Enumerable.Range(0, Utilities.LowerRussianAlphabet.Length)
+            // map to (shift, sum)
+            .Select(shift => (shift, CalculateLeastSquaresSum(text, shift)))
+            // find pair with min sum
+            .MinBy(kvp => kvp.Item2)
+            // get shift
+            .Item1;
 
-        return Decoder.Decode(text, shift);
+        return (shiftWithMinSum, Decoder.Decode(text, shiftWithMinSum));
     }
     
-    // using least squares method
-    private static double CalculateShiftValueProbability(string text, int shift)
+    private static double CalculateLeastSquaresSum(string text, int shift)
     {
-        var charToFrequency = CalculateCharFrequencies(text);
+        // for every char get their relative frequencies in text
+        var charToRelFrequency = CalculateCharRelFrequencies(text);
 
-        var leastSquaresSum = charToFrequency
+        // ∑(GlobalFrequency_i - InputShiftFrequency_i)²
+        var leastSquaresSum = charToRelFrequency
             .Select(kvp => 
                 Math.Pow(
+                    // relative frequency of kvp.Key in russian text
                     Utilities.LowerRussianCharToFrequencyInText[kvp.Key] -
-                    charToFrequency[Utilities.Shift(kvp.Key, shift, Utilities.LowerRussianAlphabet)], 
+                    // relative frequency of shifted kvp.Key in input 
+                    charToRelFrequency[Utilities.Shift(kvp.Key, shift, Utilities.LowerRussianAlphabet)], 
                     2))
             .Sum();
 
         return leastSquaresSum;
     }
     
-    private static Dictionary<char, double> CalculateCharFrequencies(string text)
+    private static Dictionary<char, double> CalculateCharRelFrequencies(string text)
     {
-        var length = text.Length;
-        var charToCount = Utilities.LowerRussianAlphabet
-            .ToDictionary(ch => ch, ch => text.Count(x => x == ch))
-            .ToDictionary(kvp => kvp.Key, kvp => (double)kvp.Value / length);
+        // length of text with only russian chars
+        var length = text.Count(c => Utilities.LowerRussianAlphabet.Contains(c));
+
+        var charToRelFrequency = Utilities.LowerRussianAlphabet
+            .ToDictionary(c => c, c => (double)text.Count(x => x == c) / length);
         
-        return charToCount;
+        return charToRelFrequency;
     }
 }
